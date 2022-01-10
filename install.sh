92817f4414be257315633ed6390b8ef5ebbf92d7
ZShab Niba <nibazshab@gmail.com>
2022-01-10 17:54:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "deb [trusted=yes] https://nibazshab.github.io/404/sourc/ termux extras" >> $PREFIX/etc/apt/sources.list
+echo "deb [trusted=yes] https://nibazshab.github.io/when/sourc/ termux extras" >> $PREFIX/etc/apt/sources.list
 pkg in when
 echo "输入 when 启动"
 exit
